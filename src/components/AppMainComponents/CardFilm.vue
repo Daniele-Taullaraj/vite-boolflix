37d9d5137828a0c9d526bd403ea9601f3bd0c5d1
Daniele-Taullaraj <danieletaullaraj@gmail.com>
2024-05-30 22:44:51 +0200

    M4

--- src/components/AppMainComponents/CardFilm.vue
+++ src/components/AppMainComponents/CardFilm.vue
@@ -22,33 +22,38 @@ export default {
     <div class="col-2 p-2 my-2">
         <div class="card bg-dark border border-2 text-white position-relative">
 
-            <!-- <div class="img-card w-100 h-100 position-relative z-2">
+            <div class="img-card w-100 h-100 position-relative z-2">
                 <img :src="movie.poster_path ? 'https://image.tmdb.org/t/p/original' + movie.poster_path : 'https://picsum.photos/300/300'"
                     alt="" class="w-100 h-100 rounded">
-            </div> -->
+            </div>
 
-            <div class="p-2 h-100 rounded position-absolute z-1">
+            <div class="p-2 h-100 rounded position-absolute z-1 overflow-auto">
                 <div>
-                    <span class="fw-bold">Titolo: </span>
+                    <span class="fw-bold text-danger">Titolo: </span>
                     <span>{{ movie.title }}</span>
                 </div>
 
                 <div>
-                    <span class="fw-bold">Titolo originale: </span>
+                    <span class="fw-bold text-danger">Titolo originale: </span>
                     <span>{{ movie.original_title }}</span>
                 </div>
 
                 <div>
-                    <span class="fw-bold">Lingua: </span>
+                    <span class="fw-bold text-danger">Lingua: </span>
                     <span class="me-2">{{ movie.original_language }}</span>
                     <img :src="`https://flagcdn.com/16x12/${movie.original_language}.png`" alt="">
 
                 </div>
 
                 <div>
-                    <span class="fw-bold">Voto : </span>
+                    <span class="fw-bold text-danger">Voto: </span>
                     <span class="me-2">{{ movie.vote_average }} </span>
                     <i v-for="star in movie.vote_average" class="fa-solid fa-star text-warning"></i>
+                </div>
+
+                <div>
+                    <span class="fw-bold text-danger">Overview: </span>
+                    <span class="me-2">{{ movie.overview }} </span>
                 </div>
             </div>
 
